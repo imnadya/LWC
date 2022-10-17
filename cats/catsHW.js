b633a9817c7f551e6a3d40dfabd91a4e882254be
imnadya <111196866+imnadya@users.noreply.github.com>
2022-10-17 16:33:59 +0300

    Create catsHW.js

---
+++ cats/catsHW.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class CatsHW extends LightningElement {
+    dogs=[
+        {id:1, url: 'https://img.freepik.com/free-vector/cute-shiba-inu-dog-astronaut-sitting-cartoon-vector-icon-illustration-animal-science-icon-isolated_138676-5799.jpg'},
+    {id:2, url: 'https://img.freepik.com/free-vector/cute-shiba-inu-dog-doing-yoga-meditation-cartoon-vector-icon-illustration-animal-healthy-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3919.jpg'},
+{id:3, url: 'https://img.freepik.com/free-vector/cute-husky-dog-sitting-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-premium_138676-4567.jpg'},    
+{id:4, url: 'https://img.freepik.com/free-vector/cute-shiba-inu-dog-hug-bone-cartoon-vector-icon-illustration-animal-food-icon-concept-isolated-flat_138676-6096.jpg'},
+{id:5, url: 'https://img.freepik.com/free-vector/cute-dog-sitting-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-flat_138676-4474.jpg'}]
+}
