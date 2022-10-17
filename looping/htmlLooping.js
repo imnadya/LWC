6afc49c9ba1f57bc6235c1d71d1799c80802821a
imnadya <111196866+imnadya@users.noreply.github.com>
2022-10-17 12:37:00 +0300

    Create htmlLooping.js

---
+++ looping/htmlLooping.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+students=[
+    {id:1, name:'Bertur'},
+    {id:2, name:'Batina'},
+    {id:3, name:'Bibigul'},
+    {id:4, name:'Nadya'},
+    {id:5, name:'Fidan'}]
+export default class HtmlLooping extends LightningElement {}
